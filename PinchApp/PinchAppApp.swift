3dbf68861bf13b774204ec4c2256b64828f36da3
hackwithrama <rajram92@hotmail.com>
2024-06-28 21:31:58 +0530

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Ramachandran Varadaraju on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
